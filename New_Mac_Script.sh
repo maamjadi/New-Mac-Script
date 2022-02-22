f9a095ec3fe14e31e9cff9db966d7ba1e935474e
Amin Amjadi <ma.amjadi@icloud.com>
2022-02-22 15:29:08 +0100

    updating the script

--- New_Mac_Script.sh
+++ New_Mac_Script.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 # Homebrew
+printf "✅ Homebrew Installation"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # oh-my-zsh Terminal
+printf "✅ oh-my-zsh Installation"
 brew install zsh
+printf "⛔️ Navigate to directory where New_Mac_Script.sh is Located on terminal!!"
+printf "-> e.g. cd [MY PATH]!!"
+printf "✅ oh-my-zsh Plugin Setup"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+printf "✅ oh-my-zsh Font Setup"
 git clone https://github.com/powerline/fonts.git --depth=1
 cd fonts
 ./install.sh
 cd ..
 rm -rf fonts
 
+printf "✅ Replace .zshrc"
+cp -f ~/.zshrc .zshrc
+
 # Packages
+printf "✅ Packages Installation"
 brew install jenv
 brew install pyenv
 brew install rbenv
@@ -41,10 +51,12 @@ brew install mint
 brew install sass/sass/sass
 
 # Gems
+printf "✅ Gems Installation"
 gem install fastlane
 gem install xcode-install
 
 # Apps
+printf "✅ Apps Installation"
 brew install skype
 brew install spotify
 brew install google-chrome
@@ -71,13 +83,15 @@ brew install qlvideo
 brew install qlmarkdown
 
 # iOS
-xcversion install 12.5
+printf "✅ iOS Setup"
+xcversion install 13.2.1
 
 sudo xcode-select -switch /Applications/Xcode.app/Contents/Developer
 
 export COCOAPODS_DISABLE_STATS=true
 
 # Android
+printf "✅ Android Setup"
 mkdir -p $HOME/.gradle && touch $HOME/.gradle/gradle.properties
 cp -v $HOME/.gradle/gradle.properties $HOME/.gradle/gradle.properties-macosi_$(date +%s)
 echo "org.gradle.daemon=true" > $HOME/.gradle/gradle.properties
@@ -91,6 +105,7 @@ export PATH=$PATH:~/Library/Android/sdk/platform-tools
 export ANDROID_HOME=~/Library/Android/sdk
 
 #Frontend
+printf "✅ Frontend Setup"
 mkdir -p $HOME/.nvm
 export NVM_DIR="$HOME/.nvm" && . /usr/local/opt/nvm/nvm.sh && nvm install node
 
